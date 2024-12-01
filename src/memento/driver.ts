9aa09f75f4b563d236ec50c55556834f366799f7
Travis Craft <tlcraft@users.noreply.github.com>
2024-12-01 13:02:26 -0700

    Add state examples.

--- src/memento/driver.ts
+++ src/memento/driver.ts
@@ -1,7 +1,7 @@
 import { StateOriginator } from "./classes/state-originator";
 
 const caretakerDriver = (): void => {
-    const state = { id: 1234, data: "Initial data" };
+    const state = { id: 1234, data: { details: "Initial data" } };
     const stateOriginator = new StateOriginator(state);
     console.log('Initial state: ', stateOriginator.state);
 
@@ -9,14 +9,24 @@ const caretakerDriver = (): void => {
 
     mementos.push(stateOriginator.createMemento());
 
-    stateOriginator.state = { id: 1234, data: "Changed data" };
+    stateOriginator.state = { id: 1234, data: { details: "Changed data" } };
     console.log('New state: ', stateOriginator.state);
+
+    mementos.push(stateOriginator.createMemento());
+
+    stateOriginator.state = { id: 1234, data: { details: "Changed data again", type: "edit" } };
+    console.log('Next state: ', stateOriginator.state);
 
     mementos.push(stateOriginator.createMemento());
 
     stateOriginator.setMemento(mementos[0]);
 
     console.log('Final state: ', stateOriginator.state);
+
+    console.log('Mementos: \n')
+    mementos.forEach((memento, index) => 
+        console.log(`Memento ${index}: `, JSON.stringify(memento))
+    )
 };
 
 caretakerDriver();
