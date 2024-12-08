cd53cd1c91ae7817f8311c8e83770080af27cc91
Travis Craft <tlcraft@users.noreply.github.com>
2024-12-08 12:06:10 -0700

    Add vehicle parameter.

--- src/mediator/interfaces/mediator.interface.ts
+++ src/mediator/interfaces/mediator.interface.ts
@@ -1,5 +1,6 @@
 import { Message } from "./message.interface";
+import { Vehicle } from "./vehicle.interface";
 
 export interface IMediator {
-    notify(message: Message): void;
+    notify(sender: Vehicle, message: Message): void;
 }
