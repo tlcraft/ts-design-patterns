3ec32fd46cb7cc084a3423c94c3c651a5988c063
Travis Craft <tlcraft@users.noreply.github.com>
2024-12-13 17:49:12 -0700

    Add driver.

---
+++ src/mediator/driver.ts
@@ -0,0 +1,17 @@
+import { Airplane } from "./classes/airplane";
+import { AirportMediator } from "./classes/airport-mediator";
+import { Helicopter } from "./classes/helicopter";
+
+const driver = (): void => {
+    const airplane = new Airplane();
+    const helicopter = new Helicopter();
+    const airTrafficControl = new AirportMediator(airplane, helicopter);
+
+    const message = {
+        data: 'METAR KSEA 081853Z 18007KT 10SM OVC013 07/05 A3022 RMK AO2 SLP241 T00720050'
+    };
+
+    airTrafficControl.notify(airplane, message);
+};
+
+driver();
