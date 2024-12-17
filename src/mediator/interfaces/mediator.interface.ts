d283be0438e2a208e77fbcf6c03312c4dd5ec2ca
Travis Craft <tlcraft@users.noreply.github.com>
2024-12-17 15:00:12 -0700

    Update mediator.

--- src/mediator/interfaces/mediator.interface.ts
+++ src/mediator/interfaces/mediator.interface.ts
@@ -1,6 +1,7 @@
+import { MessageType } from "../enums/message-type.enum";
 import { Message } from "./message.interface";
 import { Vehicle } from "./vehicle.interface";
 
 export interface IMediator {
-    notify(sender: Vehicle, message: Message): void;
+    notify(sender: Vehicle, message: Message,  messageType: MessageType): void;
 }
