15f4abdb3e3139539077480f79e89087b49ddc3b
Travis Craft <tlcraft@users.noreply.github.com>
2024-12-18 20:02:32 -0700

    Update mediator.

--- src/mediator/classes/airport-mediator.ts
+++ src/mediator/classes/airport-mediator.ts
@@ -1,3 +1,4 @@
+import { MessageType } from "../enums/message-type.enum";
 import { IMediator } from "../interfaces/mediator.interface";
 import { Message } from "../interfaces/message.interface";
 import { Vehicle } from "../interfaces/vehicle.interface";
@@ -5,13 +6,25 @@ import { Vehicle } from "../interfaces/vehicle.interface";
 export class AirportMediator implements IMediator {
     public constructor(private airplane: Vehicle, private helicopter: Vehicle) {}
 
-    notify(sender: Vehicle, message: Message): void {
-        if (sender === this.airplane) {
-            this.helicopter.operation(message);
-        }
-
-        if (sender === this.helicopter) {
-            this.airplane.operation(message);
+    notify(sender: Vehicle, message: Message, messageType: MessageType): void {
+        switch (messageType) {
+            case MessageType.Weather:
+                if (sender === this.airplane) {
+                    this.helicopter.operation(message);
+                }
+        
+                if (sender === this.helicopter) {
+                    this.airplane.operation(message);
+                }
+                break;
+            case MessageType.Location:
+                const response = {
+                    data: `${typeof sender} Seattle Tower, roger. Expect runway 1A. Maintain 3,500 feet, and report 5 miles south.`
+                };
+                sender.operation(response);
+                break;
+            default:
+                console.error('Message type not defined.');
         }
     }
 }
