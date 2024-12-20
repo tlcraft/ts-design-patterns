f5a885420b04c6e902f77404c4b76cc40c242839
Travis Craft <tlcraft@users.noreply.github.com>
2024-12-20 16:00:59 -0700

    Update driver.

--- src/mediator/driver.ts
+++ src/mediator/driver.ts
@@ -1,18 +1,26 @@
 import { Airplane } from "./classes/airplane";
 import { AirportMediator } from "./classes/airport-mediator";
 import { Helicopter } from "./classes/helicopter";
+import { MessageType } from "./enums/message-type.enum";
 
 const driver = (): void => {
     const airplane = new Airplane();
     const helicopter = new Helicopter();
     const airTrafficControl = new AirportMediator(airplane, helicopter);
 
-    const message = {
+    const weatherMessage = {
         data: 'METAR KSEA 081853Z 18007KT 10SM OVC013 07/05 A3022 RMK AO2 SLP241 T00720050'
     };
 
-    airTrafficControl.notify(airplane, message);
-    airTrafficControl.notify(helicopter, message);
+    airTrafficControl.notify(airplane, weatherMessage, MessageType.Weather);
+    airTrafficControl.notify(helicopter, weatherMessage, MessageType.Weather);
+
+    const postitionMessage = {
+        data: 'Seattle Tower, Cessna 123AB, 10 miles south of the airport, at 3,500 feet, inbound for landing with information Charlie.'
+    }
+
+    airTrafficControl.notify(airplane, postitionMessage, MessageType.Location);
+    airTrafficControl.notify(helicopter, postitionMessage, MessageType.Location);
 };
 
 driver();
