634dad4545f01ce24c044e26c550faade384cd93
Travis Craft <tlcraft@users.noreply.github.com>
2024-12-24 12:49:16 -0700

    Update mediator.

--- src/mediator/classes/airport-mediator.ts
+++ src/mediator/classes/airport-mediator.ts
@@ -4,24 +4,46 @@ import { Message } from "../interfaces/message.interface";
 import { Aircraft } from "../interfaces/aircraft.interface";
 
 export class AirportMediator implements IMediator {
-    public constructor(private airplane: Aircraft, private helicopter: Aircraft) {}
+    public aircrafts: Record<string, Aircraft> = {};
+    public constructor() {}
+
+    addAircraft(aircraft: Aircraft): void {
+        this.aircrafts[`${aircraft.constructor.name}`] = aircraft;
+    }
 
     notify(sender: Aircraft, message: Message, messageType: MessageType): void {
         switch (messageType) {
             case MessageType.Weather:
-                if (sender === this.airplane) {
-                    this.helicopter.operation(message);
+                if (sender.constructor.name === 'Airplane') {
+                    const helicopter = this.aircrafts['Helicopter'];
+                    helicopter.operation(message);
                 }
         
-                if (sender === this.helicopter) {
-                    this.airplane.operation(message);
+                if (sender.constructor.name === 'Helicopter') {
+                    const airplane = this.aircrafts['Airplane'];
+                    airplane.operation(message);
                 }
                 break;
             case MessageType.Location:
-                const response = {
+                const locationResponse = {
                     data: `${sender.constructor.name}, Seattle Tower, roger. Expect runway 1A. Maintain 3,500 feet, and report 5 miles south.`
                 };
-                sender.operation(response);
+                sender.operation(locationResponse);
+                break;
+            case MessageType.Update:
+                const updateResponse = {
+                    data: `Seattle Tower, roger. ${sender.constructor.name} has updated their coordinates.`
+                };
+
+                if (sender.constructor.name === 'Airplane') {
+                    const helicopter = this.aircrafts['Helicopter'];
+                    helicopter.operation(updateResponse);
+                }
+        
+                if (sender.constructor.name === 'Helicopter') {
+                    const airplane = this.aircrafts['Airplane'];
+                    airplane.operation(updateResponse);
+                }
                 break;
             default:
                 console.error('Message type not defined.');
